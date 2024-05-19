a5fde5ea612235908026681e63b1c8ac34424a65
DanielTTK <danielsherif10@gmail.com>
2024-05-19 22:39:22 +0200

    added revenue view files

---
+++ se/kth/iv1350/sem3/view/TotalRevenueFileOutput.java
@@ -0,0 +1,8 @@
+package se.kth.iv1350.sem3.view;
+
+/**
+ * Prints income to file
+ */
+public class TotalRevenueFileOutput {
+
+}
---
+++ se/kth/iv1350/sem3/view/TotalRevenueView.java
@@ -0,0 +1,8 @@
+package se.kth.iv1350.sem3.view;
+
+/**
+ * shows total income recieved through observers.
+ */
+public class TotalRevenueView {
+
+}
